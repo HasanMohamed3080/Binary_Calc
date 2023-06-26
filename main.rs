370f7e07e6cf2c411e1fb10fa10c4ad3cbf265b4
Hasan <137738156+HasanMohamed3080@users.noreply.github.com>
2023-06-26 04:53:52 +0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,15 +1,12 @@
 use std::io::{self, Write};
 
 fn main() {
-    // إنشاء متغير لاحتواء الرقم الذي يقوم المستخدم بإدخاله
     let mut input = String::new();
 
-    // استقبال الرقم أو النص من المستخدم
     print!("Enter a decimal number or text: ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
-    // تحويل الرقم أو النص إلى نظام الأعداد الثنائية
     let binary_num = match input.trim().parse::<u64>() {
         Ok(num) => format!("{:b}", num),
         Err(_) => {
@@ -21,7 +18,6 @@ fn main() {
         }
     };
 
-    // عرض الناتج على الشاشة
     if binary_num.is_empty() {
         println!("Invalid input");
     } else {
